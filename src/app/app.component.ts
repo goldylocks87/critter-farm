e010d1a972487a2b3af98759376d5c603259ac93
goldylocks87 <ahhlick@yahoo.com>
2018-01-04 22:26:54 -0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { DnaService } from './services/dna.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  title = 'critter farm';
+  eyes: string;
+
+  constructor( private dnaService: DnaService ) {}
+
+  ngOnInit() {
+
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    this.eyes = this.dnaService.createPunnet( value.mom, value.dad );
+  }
+
+  getEyeColor() {
+    if( !this.eyes ) return 'black';
+    else return this.eyes;
+  }
+
 }
---
+++ src/app/services/dna.service.ts
@@ -0,0 +1,65 @@
+
+export class DnaService {
+
+  critter1 = {
+    eyeGene: 'Aa'
+  };
+
+  critter2 = {
+    eyeGene: 'aa'
+  };
+
+  outcomes;
+
+  createPunnet( mom: string, dad: string ) {
+
+    this.outcomes = [];
+
+    this.critter1.eyeGene = mom;
+    this.critter2.eyeGene = dad;
+
+    this.outcomes.push( this.critter1.eyeGene.substring(0, 1) + this.critter2.eyeGene.substring(0, 1) );
+    this.outcomes.push( this.critter2.eyeGene.substring(0, 1) + this.critter1.eyeGene.substring(1, 2) );
+    this.outcomes.push( this.critter2.eyeGene.substring(1, 2) + this.critter1.eyeGene.substring(1, 2) );
+    this.outcomes.push( this.critter1.eyeGene.substring(0, 1) + this.critter2.eyeGene.substring(1, 2) );
+
+    console.log( 'punnett square :', this.outcomes);
+
+    const upperCase = this.critter1.eyeGene.substring(0, 1).toUpperCase();
+    let babes = [];
+
+    for( let i = 1; i <= 100000; i++ ) {
+
+      let babe = {
+        dna: this.outcomes[ Math.floor(Math.random() * this.outcomes.length) ],
+        eyes: null
+      };
+
+      babes.push(babe);
+
+      if( babe.dna.indexOf(upperCase) !== -1 ) { babe.eyes = 'Indigo'; }
+      else { babe.eyes = 'MediumBlue'; }
+
+    }
+
+    let blueEyes = 0;
+    let indigoEyes = 0;
+
+    for( let babe of babes ) {
+      if( babe.eyes === 'MediumBlue' ) {
+        blueEyes++;
+      } else indigoEyes++;
+    }
+
+    console.log( 'blue eyes :', blueEyes, blueEyes / babes.length * 100 + '%' );
+    console.log( 'indigo eyes :', indigoEyes, indigoEyes / babes.length * 100 + '%' );
+
+    console.log( 'YOUR BABE :', babes[0] );
+    console.log( '' );
+    console.log( '~~~~~' );
+    console.log( '' );
+
+    return babes[0].eyes;
+  }
+
+}
